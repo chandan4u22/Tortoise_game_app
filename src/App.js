e241743a5f4e9b0fe4e334cd96204c51c73b2f1e
Chandan Kumar <76109087+chandan4u22@users.noreply.github.com>
2022-07-15 11:26:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Homepage from './pages/Homepage'
+
+function App() {
+
+	return (
+		<>
+			<Container className="text-center main">
+				<Homepage />
+			</Container>
+		</>
+	)
+}
+
+export default App
